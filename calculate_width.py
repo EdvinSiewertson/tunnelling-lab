32a274b9921a371d519a00ebab663260df0e9e02
Edvin Siewertson <edvin.siewertson@gmail.com>
2023-12-14 13:31:45 +0100

    Update calculate_width.py

--- calculate_width.py
+++ calculate_width.py
@@ -3,15 +3,13 @@ from sympy import symbols, tan, atan, sqrt, solve
 # Define the symbol
 m, V0, hbar, e, b, E = symbols('m V0 hbar e b E')
 
-# Assign specific values to other variables
-m = 0.067*9.109382e-31  # m_e in kg (effective mass)
-e = 1.602176634e-19  # eU*0.3 (0.15 for Sample B)
-V0 = 0.200 * e  # V0 in Joule
-hbar = 1.05457e-34
+# Assign specific values to variables
+m = 0.067*9.109382e-31  # Effective mass
+e = 1.602176634e-19
+V0 = 0.200 * e  # V0 in J
+hbar = 1.05457e-34 # in Js
 
-def double_barrier(U, factor):
-    E = e*U*factor*0.5 - V0
-
+def double_barrier(U, sample):
     # Define the equation
     equation = hbar * atan(sqrt((0.3 * U + 0.4) / (0.3 * U))) / sqrt(m * e * U * 0.3) - b
 
@@ -20,16 +18,14 @@ def double_barrier(U, factor):
 
     for sol in solutions:
         if sol.is_real and sol.evalf() > 0:       
-            if factor==0.30:
+            if sample=='A':
                 positive_solutions_2b = 2*sol.evalf(n=10)
-                print("Solution for 2b (Sample A):")
-            elif factor==0.15:
+                print("Width of Sample A:", end=' ')
+            elif sample=='B':
                 positive_solutions_2b = sol.evalf(n=10)
-                print("Solution for 2b (Sample B):")
+                print("Width of Sample B:", end=' ')
 
     print(positive_solutions_2b)
 
-double_barrier(0.3484, 0.30) # Sample A
-double_barrier(0.2935, 0.15) # Sample B
-
-
+double_barrier(0.3484, 'A') # Sample A
+double_barrier(0.2935, 'B') # Sample B
